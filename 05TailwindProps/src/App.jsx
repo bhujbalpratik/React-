b81da21c91c69356f273f68bd94867c7da1eaa8a
bhujbal pratik <pratikbhujbal743@gmail.com>
2023-12-16 19:50:53 +0530

    05TailwindProps Created

---
+++ 05TailwindProps/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Tailwind Test</h1>
+    </>
+  );
+}
+
+export default App;
