f6092ccfc220511818c55ab60f1e4f9edb351e94
bhujbal pratik <pratikbhujbal743@gmail.com>
2023-12-17 20:21:09 +0530

    05TailwindProps Modified

--- 05TailwindProps/src/App.jsx
+++ 05TailwindProps/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-600">Tailwind Test</h1>
+      <div className="flex flex-col justify-center sm:flex-row sm:mx-auto">
+        <Card
+          cardName="Pratik Bhujbal"
+          btnText="View Pratik"
+          imgSrc="https://cdn4.sharechat.com/img_851848_365362ae_1682882849265_sc.jpg?tenant=sc&referrer=tag-service&f=265_sc.jpg"
+        />
+        <Card
+          cardName="Rahul Chavan"
+          btnText="View Rahul"
+          imgSrc="https://myquotesclub.com/wp-content/uploads/2019/07/Stylish-Boy-WhatsApp-Profile-Pics.jpg"
+        />
+        <Card
+          cardName="Sayali Takalkar"
+          btnText="View Sayali"
+          imgSrc="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+        />
+      </div>
     </>
   );
 }
---
+++ 05TailwindProps/src/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Card = ({ cardName, btnText, imgSrc }) => {
+  return (
+    <>
+      <div className="relative h-[400px] w-[300px] rounded-md ml-4 ">
+        <img
+          src={imgSrc}
+          alt="Image"
+          className="z-0 h-full w-full rounded-md object-cover"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent hover:from-gray-600"></div>
+        <div className="absolute bottom-4 left-4 text-left">
+          <h1 className="text-lg font-semibold text-white">{cardName}</h1>
+          <p className="mt-2 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            debitis?
+          </p>
+          <button className="mt-2 p-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white hover:border-b-2 hover:border-gray-400 hover:rounded-md">
+            {btnText} →
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
